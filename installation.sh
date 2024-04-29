ff12d949ed22d1e8f3dd907cccccf9b2609b25bf
Rajasekhar Reddy <brreddy1407@gmail.com>
2024-04-29 13:08:11 +0530

    Variablr-declaration

--- installation.sh
+++ installation.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=&(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2...FAILLED"
+    else
+        echo "$2...SUCCESS"
+    fi
+
 
 if [ $USERID -ne 0 ]
 then
@@ -10,13 +22,10 @@ else
     echo "you are super user"
 fi
 
-dnf instal mysql -y
+dnf instal mysql -y &>>$LOGFILE
+VALIDATE "$?" "Installation of MySQL"
 
-if [ $? -eq 0 ]
-then
-    echo "Installation of mysql ...SUCCESS"
-else
-    echo "Installation of mysql ...FAILED"
-    exit 1
-fi
+dnf install git -y &>>$LOGFILE
+VALIDATE "$?" "iNSTALLATION OF GIT"
+
 echo "testing"
