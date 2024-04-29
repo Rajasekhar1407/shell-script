6faf507c07fd1ede62754684ebaafb39b79a2526
Rajasekhar Reddy <brreddy1407@gmail.com>
2024-04-29 12:29:43 +0530

    Variablr-declaration

---
+++ installation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if ($USERID -ne 0)
+then
+    echo "Please run this script with root access"
+    exit 1 # manuvally exit if error comes
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y
+
+if [$? -eq 0]
+then
+    echo "Installation of mysql ...SUCCESS"
+else
+    echo "Installation of mysql ...FAILED"
+    exit 1
+fi
