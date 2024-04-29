653ca761b19b17580823612d284f9abbf2a225ed
Rajasekhar Reddy <brreddy1407@gmail.com>
2024-04-29 12:26:16 +0530

    Variablr-declaration

--- variable-read-command.sh
+++ variable-read-command.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo "Please enter user name:"
-read -s USERNAME
-echo "Please enter password:"
+
+echo "Please enter username::"
+
+read -s USERNAME #here USERNAME is variable and -s argument is for hidden
+
+echo "Please enter password::"
+
 read -s PASSWORD
 
-echo "username is $USERNAME"
-echo "password is $PASSWORD"
+echo "Username is: $USERNAME, Password is: $PASSWORD"
